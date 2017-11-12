5419f5548b3e5cc424d212034084f77a756eea86
fred_roger <fredroger@gmail.com>
2017-11-12 14:02:03 -0500

    Change http reponse to JSON to be able to show text in a slack channel

--- handler.go
+++ handler.go
@@ -6,7 +6,23 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// SlackResponse represents a response to a slack command
+type SlackResponse struct {
+	ResponseType string `json:"response_type"`
+	Text         string `json:"text"`
+}
+
 func getQuote(c *gin.Context) {
+
+	// TODO validate request is comming from slack with a token etc. etc.
+
+	// TODO get randomly a quote from a file (json or whatever else...)
 	quote := "Quand ma petite Scandinave a vu que j'la regardait avec un peu d'bave, elle m'a dit: « glabedichlabediglabedichlacken, glambadibediglabedichwetten, glabedichlabegrodibotchiklagen, dabodjekadebotchibotchikouine ». Ça voulait dire : « Voudrais-tu m'aider à visser ma chaise Ikéa »."
-	c.String(http.StatusOK, quote)
+
+	// Build the slack reponse and return through http
+	var r SlackResponse
+	r.ResponseType = "in_channel"
+	r.Text = quote
+
+	c.JSON(http.StatusOK, &r)
 }
